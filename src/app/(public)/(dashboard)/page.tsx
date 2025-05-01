b9f2b5ed8ebf542218cbc1b8f99006dfcf8194c2
yurizzxz <yuri.alves.silva@gmail.com>
2025-05-01 17:01:00 -0300

    fix(style): layout de gráficos e dados fictícios

--- src/app/(public)/(dashboard)/page.tsx
+++ src/app/(public)/(dashboard)/page.tsx
@@ -1,4 +1,3 @@
-import { ChartAreaInteractive } from "@/components/dashboard/chart-area-interactive";
 import { ChartBarInteractive } from "@/components/dashboard/chart-bar";
 import { SectionCards } from "@/components/dashboard/section-cards";
 import { Section, Content } from "@/components/section";
@@ -7,10 +6,9 @@ import { Section, Content } from "@/components/section";
 export default function Page() {
   return (
     <Section>
-      <h1 className="text-2xl pl-6 font-semibold">Olá! Seja bem-vindo!</h1>
+      <h1 className="text-2xl pl-6 mt-1 font-semibold">Olá! Seja bem-vindo!👋</h1>
       <SectionCards />
-      <Content className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <ChartAreaInteractive />
+      <Content className="grid grid-cols-1 gap-4 ">
         <ChartBarInteractive />
       </Content>
 
--- src/components/dashboard/chart-bar.tsx
+++ src/components/dashboard/chart-bar.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { TrendingUp } from "lucide-react"
-import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts"
+import { TrendingUp } from "lucide-react";
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts";
 
 import {
   Card,
@@ -10,38 +10,40 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart"
+} from "@/components/ui/chart";
 const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
-]
+  { course: "Análise e Desenvolvimento de Sistemas (AMS)", alunos: 186 },
+  { course: "Processos Gerais (AMS)", alunos: 305 },
+  { course: "Mecatrônica Industrial", alunos: 237 },
+  { course: "Eventos", alunos: 73 },
+  { course: "Gestão da Tecnologia da Informação", alunos: 209 },
+  { course: "Gestão Empresarial", alunos: 214 },
+];
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
+  alunos: {
+    label: "Alunos",
     color: "hsl(var(--chart-1))",
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
 export function ChartBarInteractive() {
   return (
     <Card>
       <CardHeader>
         <CardTitle>Alunos por Curso</CardTitle>
-        <CardDescription>Informações coletadas no primeiro semestre</CardDescription>
+        <CardDescription>
+          Informações coletadas no ultimo ano
+        </CardDescription>
       </CardHeader>
       <CardContent>
-        <ChartContainer config={chartConfig}>
+        <ChartContainer className="aspect-auto h-[400px] w-full" config={chartConfig}>
           <BarChart
             accessibilityLayer
             data={chartData}
@@ -51,17 +53,38 @@ export function ChartBarInteractive() {
           >
             <CartesianGrid vertical={false} />
             <XAxis
-              dataKey="month"
+              dataKey="course"
               tickLine={false}
               tickMargin={10}
               axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)}
+              tickFormatter={(value: string) => {
+                const ignoredWords = ["de", "da", "do", "e", "dos"];
+                const matchSigla = value.match(/\(([^)]+)\)$/);
+                const suffix = matchSigla ? ` ${matchSigla[0]}` : "";
+                const withoutSuffix = value.replace(/\s*\([^)]+\)$/, "");
+                const words = withoutSuffix.split(" ");
+                const significantWords = words.filter(
+                  (word) => !ignoredWords.includes(word.toLowerCase())
+                );
+
+                const totalLength = withoutSuffix.length;
+
+                if (totalLength > 7) {
+                  const sigla = significantWords
+                    .map((word) => word[0]?.toUpperCase())
+                    .join(".");
+                  return `${sigla}${suffix}`;
+                }
+
+                return value;
+              }}
             />
+
             <ChartTooltip
               cursor={false}
               content={<ChartTooltipContent hideLabel />}
             />
-            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8}>
+            <Bar dataKey="alunos" fill="var(--color-primary)" radius={8}>
               <LabelList
                 position="top"
                 offset={12}
@@ -81,5 +104,5 @@ export function ChartBarInteractive() {
         </div>
       </CardFooter> */}
     </Card>
-  )
+  );
 }
